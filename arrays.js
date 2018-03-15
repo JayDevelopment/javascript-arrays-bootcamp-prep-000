e73c4facb4363378e16ebf0e45f96b5efd6d4d7b
Jordan Merrill <amanithought@gmail.com>
2018-03-15 19:27:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray (array, element) {
 }
 function destructivelyAddElementToBeginningOfArray (array, element) {
   const array2 = array.unshift()
-  return [element, array.unshift()]
+  return [element, array2]
 }
 function addElementToEndOfArray (array, element) {
   return [...array, element]
