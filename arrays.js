66c13ee278e2256cec454cd1fa43eca05f657680
Jordan Merrill <amanithought@gmail.com>
2018-03-15 20:12:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function accessElementInArray (array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-  return array.shift(element);
+  array.shift();
 }
 function removeElementFromBeginningOfArray (array) {
   return array.slice();
